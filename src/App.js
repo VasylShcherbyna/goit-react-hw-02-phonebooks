9db682cbc8dac1cabb9a22b0daf68e424a5dad01
VasylShcherbyna <75271842+VasylShcherbyna@users.noreply.github.com>
2021-11-04 18:50:01 +0100

    id

--- src/App.js
+++ src/App.js
@@ -1,8 +1,52 @@
-function App() {
-  return (
-      <h1>Hello</h1>
-  
-  );
+import { Component } from 'react';
+import shortid from 'shortid';
+
+import ContactForm from './Componnent/ContactForm';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addContact = task => {
+    const searchSameName = this.state.contacts
+      .map(cont => cont.name)
+      .includes(task.name);
+
+    if (searchSameName) {
+      alert(`${task.name} is already in contacts`);
+    } else if (task.name.length === 0) {
+      alert('Fields must be filled!');
+    } else {
+      const contact = {
+        ...task,
+        id: shortid(),
+      };
+
+      this.setState(prevState => ({
+        contacts: [...prevState.contacts, contact],
+      }));
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.Contact} />
+
+        <h2>Contacts</h2>
+        {/* <Filter />
+      <ContactList /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
