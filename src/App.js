1146b7e6d7e33e6a703dc3ab34a3e6af1cc36073
VasylShcherbyna <75271842+VasylShcherbyna@users.noreply.github.com>
2021-12-03 21:47:27 +0100

    1

--- src/App.js
+++ src/App.js
@@ -7,26 +7,21 @@ import ContactList from './Componnent/ListContact';
 import Filter from './Componnent/Filtr';
 
 function App() {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(
+    JSON.parse(window.localStorage.getItem('contacts')) ?? [],
+  );
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
-    // const contacts = localStorage.getItem('contacts');
-    // const parsedContacts = JSON.parse(contacts);
-    // setContacts(parsedContacts);
     window.localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
 
   const addContact = task => {
     const taskNameNormalized = task.name.toLowerCase();
-    const searchSameName = contacts.find(
-      contact => contact.name.toLowerCase() === taskNameNormalized,
-    );
+    const searchSameName = () =>
+      contacts.find(
+        contact => contact.name.toLowerCase() === taskNameNormalized,
+      );
 
     if (searchSameName) {
       alert(`${task.name} is already in contacts`);
@@ -46,9 +41,10 @@ function App() {
   };
 
   const normalizeFilter = filter.toLowerCase();
-  const getVisibleContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(normalizeFilter),
-  );
+  const getVisibleContacts = () =>
+    contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase()),
+    );
 
   const removeContact = contactId => {
     setContacts(contacts.filter(({ id }) => id !== contactId));
